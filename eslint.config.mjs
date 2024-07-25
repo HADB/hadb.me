c39dfcb3585fbb5e7afdcc73b1b39d0f55beadeb
Bean <dengbin@bilibili.com>
2024-07-25 20:10:05 +0800

    chore: eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import antfu from '@antfu/eslint-config'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  antfu({
+    lessOpinionated: true,
+    rules: {
+      'style/brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
+      'style/arrow-parens': ['error', 'always'],
+      'vue/no-multiple-template-root': 'off',
+    },
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
+  }),
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxt/eslint'],
   routeRules: {
     '/': { prerender: true },
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
   },
   compatibilityDate: '2024-07-25',
 })
