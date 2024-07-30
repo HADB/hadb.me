cf2310834a6ffbc4a3832390448176d295b398b2
Bean <dengbin@bilibili.com>
2024-07-30 16:44:37 +0800

    feat: 使用 titleTemplate

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 const { page } = useContent()
+useHead({
+  titleTemplate: (title) => {
+    return title ? `${title} | HADB.ME` : 'HADB.ME'
+  },
+})
 </script>
 
 <template>
   <div class="px-4 sm:px-8">
     <div class="max-w-7xl px-4 sm:px-8 py-10 m-auto bg-white dark:bg-slate-800 shadow rounded-lg ring-1 ring-slate-200 dark:ring-slate-700">
       <main class="max-w-none prose dark:prose-invert">
-        <h1 v-if="page.title">
+        <h1 v-if="page?.title">
           {{ page.title }}
         </h1>
-        <PostInfo v-if="page._dir === 'posts'" :page="page" />
+        <PostInfo v-if="page?._dir === 'posts'" :page="page" />
         <slot />
       </main>
     </div>
