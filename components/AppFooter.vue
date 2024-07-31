628c3cb02d1e958ee19674b6391a92977a6b8562
Bean <dengbin@bilibili.com>
2024-07-31 20:51:43 +0800

    feat: 样式优化

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center p-4 mt-4 text-sm text-slate-500">
+  <div class="flex items-center justify-center text-sm text-slate-500">
     <span>Copyright © 2012-{{ new Date().getFullYear() }} Bean Deng</span>
     <span class="mx-2 font-sans text-sm antialiased font-normal leading-normal pointer-events-none select-none text-blue-gray-500">|</span>
     <a class="hover:text-primary-400" target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans">CC BY-NC-SA 4.0</a>
