1a5eef378b0266271bc05b3fae006de2335a0b20
Bean <dengbin@bilibili.com>
2024-07-31 16:29:55 +0800

    feat: 自定义 checkbox 样式

---
+++ components/content/PageTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="contains-task-list test">
+    <li class="task-list-item">
+      <input type="checkbox"> 可选择的
+    </li>
+    <li class="task-list-item">
+      <input type="checkbox" checked> 可选择的选中的
+    </li>
+    <li class="task-list-item">
+      <input type="checkbox" disabled> 不可选择的
+    </li>
+    <li class="task-list-item">
+      <input type="checkbox" disabled checked> 不可选择的选中的
+    </li>
+  </ul>
+</template>
