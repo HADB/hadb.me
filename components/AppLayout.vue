c765691e46553e2872fca2b6b6b44d119f6d7673
Bean <bean@yuanfen.net>
2024-08-04 01:48:58 +0800

    feat: favicon

--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="py-4 sm:py-12">
-    <Html lang="en" />
-    <Meta property="og:image" content="/favicon.svg" />
+    <Html lang="zh-CN" />
+    <Meta property="og:image" content="/favicon-1024x1024.png" />
+    <Meta property="og:image:width" content="1024" />
+    <Meta property="og:image:height" content="1024" />
+    <Meta property="og:image:type" content="image/png" />
     <Meta name="twitter:card" content="summary_large_image" />
-    <Link rel="icon" href="/favicon.svg" />
+    <Link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <Link rel="icon" type="image/png" sizes="1024x1024" href="/favicon/favicon-1024x1024.png" />
+    <Link rel="icon" type="image/png" sizes="1024x1024" href="/favicon/favicon-1024x1024-dark.png" media="(prefers-color-scheme: dark)" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32-dark.png" media="(prefers-color-scheme: dark)" />
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16-dark.png" media="(prefers-color-scheme: dark)" />
+    <Link rel="manifest" href="/favicon/site.webmanifest" />
     <NuxtLoadingIndicator />
     <AppNavbar />
     <slot />
