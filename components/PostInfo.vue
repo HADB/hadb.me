32cbd0cae7ac990a7b64498036892b65fa4c6c75
Bean <dengbin@bilibili.com>
2024-08-08 14:02:15 +0800

    feat: post info 样式优化

--- components/PostInfo.vue
+++ components/PostInfo.vue
@@ -9,19 +9,33 @@ defineProps<Props>()
 </script>
 
 <template>
-  <div class="flex items-center flex-wrap text-sm text-slate-600 dark:text-slate-400 leading-6 divided">
-    <template v-if="page?.date">
-      <span>
-        <time :datetime="page.date">
-          {{ formatDateTime(page.date, 'yyyy 年 MM 月 dd 日') }}
-        </time>
-      </span>
-    </template>
-    <span>共 {{ page.readingTime.words }} 字</span>
-    <span>约 {{ getReadingMinutes(page.readingTime.time) }} 分钟</span>
-    <span><span class="artalk-pv-count">...</span> 阅读</span>
-    <span><span class="artalk-comment-count">...</span> 评论</span>
-    <div v-if="page.tags">
+  <div class="flex items-center flex-wrap text-sm text-slate-600 dark:text-slate-400 leading-10 divided border-t border-b border-slate-200 dark:border-slate-700">
+    <div v-if="page?.date" class="flex items-center">
+      <Icon name="uil:calendar-alt" class="w-4 h-4 mr-1" />
+      <time :datetime="page.date">
+        {{ formatDateTime(page.date, 'yyyy 年 MM 月 dd 日') }}
+      </time>
+    </div>
+    <div class="flex items-center">
+      <Icon name="uil:font" class="w-4 h-4 mr-1" />
+      <span>{{ page.readingTime.words }} 字</span>
+    </div>
+    <div class="flex items-center">
+      <Icon name="uil:clock" class="w-4 h-4 mr-1" />
+      <span>{{ getReadingMinutes(page.readingTime.time) }} 分钟</span>
+    </div>
+    <div class="flex items-center">
+      <Icon name="uil:eye" class="w-4 h-4 mr-1" />
+      <span class="artalk-pv-count">...</span>
+      <span>&nbsp;阅读</span>
+    </div>
+    <div class="flex items-center">
+      <Icon name="uil:comment-alt-lines" class="w-4 h-4 mr-1" />
+      <span class="artalk-comment-count">...</span>
+      <span>&nbsp;评论</span>
+    </div>
+    <div v-if="page.tags" class="flex items-center">
+      <Icon name="uil:tag-alt" class="w-4 h-4 mr-1" />
       <span>标签：</span>
       <template v-for="tag in page.tags" :key="tag">
         <NuxtLink :to="`/tags/${encodeURIComponent(tag)}`" class="!border-b-0">
