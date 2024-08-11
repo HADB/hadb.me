73aee1d429aec8ebc5dfda518aa98d5b9e309b5c
Bean <bean@yuanfen.net>
2024-08-11 22:02:26 +0800

    feat: add VideoPlayer

---
+++ components/content/VideoPlayer.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+interface Props {
+  src: string
+  class?: string
+  autoplay?: boolean
+  controls?: boolean
+  loop?: boolean
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <video
+    :src="props.src"
+    :class="props.class"
+    :autoplay="props.autoplay"
+    :controls="props.controls"
+    :loop="props.loop"
+  />
+</template>
