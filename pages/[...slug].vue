e78220195d3a925c9b8c2d64249eac2cb6ada672
Bean <dengbin@bilibili.com>
2024-08-12 13:48:49 +0800

    feat: server 模式下支持带日期的路径跳转

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -6,7 +6,12 @@ const { page, layout } = useContent()
 const route = useRoute()
 
 if (!(page as any).value) {
-  // 支持旧路径 301 跳转至 /posts/下
+  // 支持将旧的带日期的路径 301 跳转至 /posts/ 下
+  if (route.path.match(/^\/\d{4}\/\d{2}\/\d{2}\/(.+)$/)) {
+    navigateTo(`/posts/${route.params.slug[3]}`, { redirectCode: 301 })
+  }
+
+  // 支持旧的不带日期的路径 301 跳转至 /posts/ 下
   const { data: fallbackPost } = await useAsyncData(`fallbackPost-${route.params.slug[0]}`, () => queryContent(`posts/${route.params.slug[0]}`).only('_path').findOne())
   if (fallbackPost.value) {
     navigateTo(fallbackPost.value._path, { redirectCode: 301 })
