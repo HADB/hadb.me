08d27023563201993f7e9bedd76cb13015577967
Bean <dengbin@bilibili.com>
2024-08-15 13:09:23 +0800

    feat: add 邮箱组件，替换 `@` 符号

---
+++ components/content/Email.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  email: string
+}>()
+
+const email_prefix = props.email.split('@')[0]
+const email_suffix = props.email.split('@')[1]
+</script>
+
+<template>
+  <span class="inline-flex items-center">
+    {{ email_prefix }}
+    <Icon name="uil:at" class="mx-0.5" />
+    {{ email_suffix }}
+  </span>
+</template>
