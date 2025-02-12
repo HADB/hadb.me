a0b5e474e069ef5ccc72acc3388edea9663aa483
Bean <dengbin@bilibili.com>
2025-02-12 20:06:16 +0800

    feat: 添加打赏二维码

---
+++ components/content/RewardCode.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+interface Props {
+  width?: number
+  height?: number
+}
+
+withDefaults(defineProps<Props>(), {
+  width: 512,
+  height: 512,
+})
+</script>
+
+<template>
+  <NuxtImg :width="width" :height="height" src="/images/reward-code.jpg" />
+</template>
