9ef514b747ff11a863e59153117b8fe49f7afa68
Bean <dengbin@bilibili.com>
2025-02-20 11:09:28 +0800

    feat: 自动生成 redirects

--- redirects.ts
+++ redirects.ts
@@ -1,42 +1,38 @@
-// 支持在 generate 阶段生成重定向配置，为了在免费版 CloudFlare Pages 上实现重定向功能
+import fs from 'node:fs'
+import path from 'node:path'
 
-const redirects: { [path: string]: { redirect: { to: string, statusCode?: number } } } = {
-  '/posts/display-deepseek-r1-thinking': { redirect: { to: '/posts/article/2025/display-deepseek-r1-thinking', statusCode: 301 } },
-  '/posts/fastboot-failed-remote-operation-not-permitted': { redirect: { to: '/posts/article/2021/fastboot-failed-remote-operation-not-permitted', statusCode: 301 } },
-  '/posts/bad-request-invalid-hostname': { redirect: { to: '/posts/article/2016/bad-request-invalid-hostname', statusCode: 301 } },
-  '/posts/deploy-deepseek-r1-for-free': { redirect: { to: '/posts/article/2025/deploy-deepseek-r1-for-free', statusCode: 301 } },
-  '/posts/synology-letsencrypt-multiple-domain-cert-configuration': { redirect: { to: '/posts/article/2021/synology-letsencrypt-multiple-domain-cert-configuration', statusCode: 301 } },
-  '/posts/k8s-cert-manager-tls': { redirect: { to: '/posts/article/2020/k8s-cert-manager-tls', statusCode: 301 } },
-  '/posts/how-to-delete-web-service-of-synology-dsm-7-2': { redirect: { to: '/posts/article/2024/how-to-delete-web-service-of-synology-dsm-7-2', statusCode: 301 } },
-  '/posts/solve-pycharm-adding-pipenv-error': { redirect: { to: '/posts/article/2019/solve-pycharm-adding-pipenv-error', statusCode: 301 } },
-  '/posts/android-webview-picture-cache': { redirect: { to: '/posts/article/2020/android-webview-picture-cache', statusCode: 301 } },
-  '/posts/get-video-rotation-by-mp4box-js': { redirect: { to: '/posts/article/2024/get-video-rotation-by-mp4box-js', statusCode: 301 } },
-  '/posts/change-domain-in-weixin': { redirect: { to: '/posts/article/2015/change-domain-in-weixin', statusCode: 301 } },
-  '/posts/batch-edit-acl-for-oss': { redirect: { to: '/posts/article/2020/batch-edit-acl-for-oss', statusCode: 301 } },
-  '/posts/aliyun-cdn-not-support-sni': { redirect: { to: '/posts/article/2016/aliyun-cdn-not-support-sni', statusCode: 301 } },
-  '/posts/centos-wire-ieee8021x-config': { redirect: { to: '/posts/article/2019/centos-wire-ieee8021x-config', statusCode: 301 } },
-  '/posts/shanghai-luohu': { redirect: { to: '/posts/article/2023/shanghai-luohu', statusCode: 301 } },
-  '/posts/slice-of-mind-20240801': { redirect: { to: '/posts/article/2024/slice-of-mind-20240801', statusCode: 301 } },
-  '/posts/windows-storage-spaces-raid1-degraded': { redirect: { to: '/posts/article/2018/windows-storage-spaces-raid1-degraded', statusCode: 301 } },
-  '/posts/snaps-to-device-pixels': { redirect: { to: '/posts/article/2012/snaps-to-device-pixels', statusCode: 301 } },
-  '/posts/diary-20240611': { redirect: { to: '/posts/article/2024/diary-20240611', statusCode: 301 } },
-  '/posts/switch-blog-from-ghost-to-nuxt-content': { redirect: { to: '/posts/article/2024/switch-blog-from-ghost-to-nuxt-content', statusCode: 301 } },
-  '/posts/managing-containers-in-my-homelab': { redirect: { to: '/posts/article/2024/managing-containers-in-my-homelab', statusCode: 301 } },
-  '/posts/docker-registry-auth-with-same-domain': { redirect: { to: '/posts/article/2019/docker-registry-auth-with-same-domain', statusCode: 301 } },
-  '/posts/guide-for-beginners': { redirect: { to: '/posts/article/2024/guide-for-beginners', statusCode: 301 } },
-  '/posts/gitlab-ci-auto-deploy-python-lib': { redirect: { to: '/posts/article/2023/gitlab-ci-auto-deploy-python-lib', statusCode: 301 } },
-  '/posts/sqlite-error-no-such-table': { redirect: { to: '/posts/article/2012/sqlite-error-no-such-table', statusCode: 301 } },
-  '/posts/audiowave-animation': { redirect: { to: '/posts/article/2019/audiowave-animation', statusCode: 301 } },
-  '/posts/how-to-become-a-fullstack-developer': { redirect: { to: '/posts/article/2020/how-to-become-a-fullstack-developer', statusCode: 301 } },
-  '/posts/end-of-2024': { redirect: { to: '/posts/article/2025/end-of-2024', statusCode: 301 } },
-  '/posts/devops-gitlab-ci-aliyun-k8s': { redirect: { to: '/posts/article/2020/devops-gitlab-ci-aliyun-k8s', statusCode: 301 } },
-  '/posts/thoughts-about-ai': { redirect: { to: '/posts/article/2024/thoughts-about-ai', statusCode: 301 } },
+const postsDirectory = path.join(__dirname, 'content/posts/article')
+const redirects: { [path: string]: { redirect: { to: string, statusCode?: number } } } = {}
+
+// 获取目录下所有的 .md 文件
+const getPostFiles = (dir: string): string[] => {
+  let files: string[] = []
+
+  const items = fs.readdirSync(dir)
+  items.forEach((item) => {
+    const fullPath = path.join(dir, item)
+    const stat = fs.statSync(fullPath)
+
+    if (stat.isDirectory()) {
+      files = files.concat(getPostFiles(fullPath))
+    }
+    else if (fullPath.endsWith('.md')) {
+      files.push(fullPath)
+    }
+  })
+
+  return files
 }
 
-for (const [_path, config] of Object.entries(redirects)) {
-  if (config.redirect?.to) {
-    config.redirect.to = encodeURI(config.redirect.to)
+// 遍历目录下的文件生成重定向规则
+getPostFiles(postsDirectory).forEach((filePath) => {
+  const fileName = path.basename(filePath).replace(/^\d{8}\./, '').replace('.md', '')
+  const year = path.basename(path.dirname(filePath))
+  const pathUrl = `/posts/article/${year}/${fileName}`
+
+  redirects[`/posts/${fileName}`] = {
+    redirect: { to: encodeURI(pathUrl), statusCode: 301 },
   }
-}
+})
 
 export default redirects
